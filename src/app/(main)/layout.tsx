f6e1ca0f31c4c91454fd8a4de3e9e7715a63da3e
Onur Yılmaz <Onurylmz.41@outlook.com>
2024-08-14 16:46:54 +0300

    Responsive sticky sidebar/bottom bar

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,6 +2,7 @@ import { validateRequest } from "@/auth";
 import { redirect } from "next/navigation";
 import SessionProvider from "./SessionProvider";
 import Navbar from "./Navbar";
+import MenuBar from "./MenuBar";
 
 export default async function Layout({
   children,
@@ -16,9 +17,11 @@ export default async function Layout({
     <SessionProvider value={session}>
         <div className="flex min-h-screen flex-col">
           <Navbar/>
-          <div className="max-w-7xl mx-auto p-5">
+          <div className="max-w-7xl mx-auto p-5 flex w-full grow gap-5">
+            <MenuBar className="sticky top-[5.25rem] h-fit hidden sm:block flex-none space-y-3 rounded-2xl bg-card px-3 py-5 lg:px-5 shadow-sm xl:w-80"/>
             {children}
           </div>
+          <MenuBar className="sticky bottom-0 flex w-full justify-center gap-5 border-t bg-card p-3 sm:hidden"/>
         </div>
     </SessionProvider>
   )
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      Front Page
+    <main className="h-[200vh] w-full bg-red-50">
+      <div className="w-full">
+          Front Page
+      </div>
     </main>
   )
 }
