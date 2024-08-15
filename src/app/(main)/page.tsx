e2791fca04562ab68c4b492882d1bd2ae897aa21
Onur Yılmaz <Onurylmz.41@outlook.com>
2024-08-15 09:34:28 +0300

    Creating posts (TipTap editor)

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,11 @@
+import PostEditor from "@/components/posts/editor/PostEditor";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="h-[200vh] w-full bg-red-50">
       <div className="w-full">
-          Front Page
+            <PostEditor/>
       </div>
     </main>
   )
