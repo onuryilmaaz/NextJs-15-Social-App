29a6bf79878f854fa2e83a461cd07d151be13529
Onur Yılmaz <Onurylmz.41@outlook.com>
2024-08-15 12:15:09 +0300

    Loading posts server-side (server component)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,20 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { formatDate, formatDistanceToNowStrict } from "date-fns"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function formatRelativeDate(from: Date) {
+  const  currentData = new Date();
+  if(currentData.getTime() - from.getTime() < 24 * 60 * 60 * 1000) {
+    return formatDistanceToNowStrict(from, { addSuffix: true })
+  } else {
+    if(currentData.getFullYear() === from.getFullYear()) {
+      return formatDate(from, "MMM d");
+    } else {
+      return formatDate(from, "MMM d yyyy");
+    }
+  }
 }
