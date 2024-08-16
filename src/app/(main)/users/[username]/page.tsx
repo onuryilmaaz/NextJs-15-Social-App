8f93135555ab0b4cd25bc7bb5ec71d08fd4fa890
Onur Yılmaz <Onurylmz.41@outlook.com>
2024-08-16 16:55:34 +0300

    User tooltip & react-linkify-it

--- src/app/(main)/users/[username]/page.tsx
+++ src/app/(main)/users/[username]/page.tsx
@@ -12,6 +12,7 @@ import { Metadata } from "next"
 import { notFound } from "next/navigation"
 import { cache } from "react"
 import UserPosts from "./UserPosts"
+import Linkify from "@/components/Linkify"
 
 interface PageProps {
     params: { username: string }
@@ -118,9 +119,11 @@ async function UserProfile({ user, loggedInUserId }: UserProfileProps) {
             {user.bio && (
                 <>
                     <hr />
-                    <div className="whitespace-pre-line overflow-hidden break-words">
-                        {user.bio}
-                    </div>
+                    <Linkify>
+                        <div className="whitespace-pre-line overflow-hidden break-words">
+                            {user.bio}
+                        </div>
+                    </Linkify>
                 </>
             )} 
         </div>
--- src/components/posts/Post.tsx
+++ src/components/posts/Post.tsx
@@ -6,6 +6,8 @@ import { formatRelativeDate } from "@/lib/utils";
 import Link from "next/link";
 import UserAvatar from "../UserAvatar";
 import PostMoreButton from "./PostMoreButton";
+import Linkify from "../Linkify";
+import UserTooltip from "../UserTooltip";
 
 interface PostProps {
   post: PostData;
@@ -18,16 +20,20 @@ export default function Post({ post }: PostProps) {
     <article className="group/post space-y-3 rounded-2xl bg-card p-5 shadow-sm">
       <div className="flex justify-between gap-3">
         <div className="flex flex-wrap gap-3">
-          <Link href={`/users/${post.user.username}`}>
-            <UserAvatar avatarUrl={post.user.avatarUrl} />
-          </Link>
+          <UserTooltip user={post.user}>
+            <Link href={`/users/${post.user.username}`}>
+              <UserAvatar avatarUrl={post.user.avatarUrl} />
+            </Link>
+          </UserTooltip>
           <div>
+          <UserTooltip user={post.user}>
             <Link
               href={`/users/${post.user.username}`}
               className="block font-medium hover:underline"
             >
               {post.user.displayName}
             </Link>
+          </UserTooltip>
             <Link
               href={`/posts/${post.id}`}
               className="block text-sm text-muted-foreground hover:underline"
@@ -43,7 +49,9 @@ export default function Post({ post }: PostProps) {
           />
         )}
       </div>
-      <div className="whitespace-pre-line break-words">{post.content}</div>
+      <Linkify>
+        <div className="whitespace-pre-line break-words">{post.content}</div>
+      </Linkify>
     </article>
   );
 }
