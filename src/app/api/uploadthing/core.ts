90833d81c5ca1aa7e88f44c59743c8c0c1a62c0b
Onur Yılmaz <Onurylmz.41@outlook.com>
2024-08-19 15:17:22 +0300

    Post media uploads (image & video)

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -30,7 +30,7 @@ export const fileRouter = {
         const newAvatarUrl = file.url.replace(
             "/f/",
             `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/`
-        )  
+        );  
         await prisma.user.update({
             where: { id: metadata.user.id },
             data: {
@@ -39,6 +39,30 @@ export const fileRouter = {
         });
         return { avatarUrl: newAvatarUrl }
     }),
+    attachment: f({
+        image: { maxFileSize: "4MB", maxFileCount: 5 },
+        video: { maxFileSize: "64MB", maxFileCount: 5 }
+    })
+    .middleware(async () => {
+        const { user } = await validateRequest();
+
+        if(!user) throw new UploadThingError("Unauthorized");
+
+        return {}
+    })
+    .onUploadComplete(async ({ file }) => {
+        const media = await prisma.media.create({
+            data: {
+                url: file.url.replace(
+                    "/f/",
+                    `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/`
+                ),
+                type: file.type.startsWith("image") ? "IMAGE" : "VIDEO"
+            }
+        });
+
+        return { mediaId: media.id };
+    })
 } satisfies FileRouter;
 
 export type AppFileRouter = typeof fileRouter;
--- src/components/posts/Post.tsx
+++ src/components/posts/Post.tsx
@@ -2,12 +2,14 @@
 
 import { useSession } from "@/app/(main)/SessionProvider";
 import { PostData } from "@/lib/types";
-import { formatRelativeDate } from "@/lib/utils";
+import { cn, formatRelativeDate } from "@/lib/utils";
+import { Media } from "@prisma/client";
+import Image from "next/image";
 import Link from "next/link";
-import UserAvatar from "../UserAvatar";
-import PostMoreButton from "./PostMoreButton";
 import Linkify from "../Linkify";
+import UserAvatar from "../UserAvatar";
 import UserTooltip from "../UserTooltip";
+import PostMoreButton from "./PostMoreButton";
 
 interface PostProps {
   post: PostData;
@@ -26,14 +28,14 @@ export default function Post({ post }: PostProps) {
             </Link>
           </UserTooltip>
           <div>
-          <UserTooltip user={post.user}>
-            <Link
-              href={`/users/${post.user.username}`}
-              className="block font-medium hover:underline"
-            >
-              {post.user.displayName}
-            </Link>
-          </UserTooltip>
+            <UserTooltip user={post.user}>
+              <Link
+                href={`/users/${post.user.username}`}
+                className="block font-medium hover:underline"
+              >
+                {post.user.displayName}
+              </Link>
+            </UserTooltip>
             <Link
               href={`/posts/${post.id}`}
               className="block text-sm text-muted-foreground hover:underline"
@@ -52,6 +54,60 @@ export default function Post({ post }: PostProps) {
       <Linkify>
         <div className="whitespace-pre-line break-words">{post.content}</div>
       </Linkify>
+      {!!post.attachments.length && (
+        <MediaPreviews attachments={post.attachments} />
+      )}
     </article>
   );
+}
+
+interface MediaPreviewsProps {
+  attachments: Media[];
+}
+
+function MediaPreviews({ attachments }: MediaPreviewsProps) {
+  return (
+    <div
+      className={cn(
+        "flex flex-col gap-3",
+        attachments.length > 1 && "sm:grid sm:grid-cols-2",
+      )}
+    >
+      {attachments.map((m) => (
+        <MediaPreview key={m.id} media={m} />
+      ))}
+    </div>
+  );
+}
+
+interface MediaPreviewProps {
+  media: Media;
+}
+
+function MediaPreview({ media }: MediaPreviewProps) {
+  if (media.type === "IMAGE") {
+    return (
+      <Image
+        src={media.url}
+        alt="Attachment"
+        width={500}
+        height={500}
+        className="mx-auto size-fit max-h-[30rem] rounded-2xl"
+      />
+    );
+  }
+
+  if (media.type === "VIDEO") {
+    return (
+      <div>
+        <video
+          src={media.url}
+          controls
+          className="mx-auto size-fit max-h-[30rem] rounded-2xl"
+        />
+      </div>
+    );
+  }
+
+  return <p className="text-destructive">Unsupported media type</p>;
 }
